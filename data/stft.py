efb74b7e2aa45d83a27e88c11a36208dea886464
jacopo <jacopo.fichera98@gmail.com>
2025-05-29 21:53:25 +0200

    WIP

---
+++ data/stft.py
@@ -0,0 +1,44 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import signal
+from scipy.stats import stats
+
+
+def compute_stft(x, fs: float, clip_frequency: int = -1, **kwargs):
+    f, t, z_xx = signal.stft(x, fs, **kwargs)
+    # f and z_xx are sorted in ascending order
+
+    clip_fs = np.argmax(f > clip_frequency) if clip_frequency > 0 else -1
+
+    # Do clipping operation to discard high frequencies that may be noisy or irrelevant
+    # (e.g., beyond physiological range).
+    z_xx = np.abs(z_xx[:clip_fs])  # Just take the magnitude
+    f = f[:clip_fs]
+
+    if np.isnan(z_xx).any():
+        raise ValueError('stft returned NaN')
+
+    return f, t, z_xx
+
+
+def normalize_stft_zscore(f, t, z_xx, **kwargs):
+    clip = 5  # To handle boundary effects
+    z_xx = z_xx[:, clip:-clip]
+    return f, t[clip:-clip], stats.zscore(z_xx, axis=-1)
+
+
+def plot(f, t, z_xx):
+    plt.figure(figsize=(15, 3))
+    g1 = plt.pcolormesh(t, f, z_xx, shading="gouraud", vmin=-3, vmax=5)
+
+    cbar = plt.colorbar(g1)
+    tick_font_size = 15
+    cbar.ax.tick_params(labelsize=tick_font_size)
+    cbar.ax.set_ylabel("Power (Arbitrary units)", fontsize=15)
+
+    plt.xlabel("Time (s)", fontsize=20)
+    plt.xticks(fontsize=20)
+
+    plt.ylabel("Frequency (Hz)", fontsize=20)
+    plt.yticks(fontsize=20)
+    return plt
