0ce394c79fbf9d38c7b10f40a708fca4fe39b9a1
jacopo <jacopo.fichera98@gmail.com>
2025-08-25 22:48:52 +0200

    Next steps for VATE

--- models/FEEG/trainer.py
+++ models/FEEG/trainer.py
@@ -1,42 +1,39 @@
 import torch
-from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
 from models.FEEG.model import EEGAVI
+from models.VATE.constrastive_model import ContrastiveModel
 
 
+# todo
 # ad hoc for this instance?
 class EEGAVIKDTrainer:
-    def __init__(self, model: EEGAVI, teacher: nn.Module, loss_fn, learning_rate: float = 1e-3):
+    def __init__(self, model: EEGAVI, teacher: ContrastiveModel, loss_fn, learning_rate: float = 1e-3):
         self.model: EEGAVI = model
-        self.teacher: nn.Module = teacher
+        self.teacher: ContrastiveModel = teacher
+
         self.loss_fn = loss_fn
         self.optimizer: torch.optim.Optimizer = Adam(self.model.parameters(), lr=learning_rate)
 
-    def train(self, train_loader: DataLoader, val_loader: DataLoader = None, epochs: int = 20):
+    # todo train loader deve contenre anche records per teacher
+    def train(self, train_loader: DataLoader, epochs: int = 20):
         self.model.train()
         for epoch in range(epochs):
             running_loss = .0
-            for x in train_loader:
+            for x, teacher_x in train_loader:
                 self.optimizer.zero_grad()
 
                 with torch.no_grad():
-                    x_teacher = self.adapt_input_for_teacher()
-                    logits = self.teacher(x_teacher)
+                    logits = self.teacher(teacher_x)
 
                 outputs, kd_objects = self.model(x)
-                # TODO
+                # TODO: I can apply CLIP-loss
                 loss = self.loss_fn(outputs, x)
+
                 loss.backward()
                 self.optimizer.step()
 
                 running_loss += loss.item()
 
             print(f"Epoch {epoch + 1}/{epochs}, Loss: {running_loss / len(train_loader)}")
-
-    def adapt_input_for_teacher(self):
-        pass
-
-    def test(self):
-        pass
