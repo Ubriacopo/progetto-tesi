21d781c2dd25e6a59721a3a315a7c3c137e987ef
jacopo <jacopo.fichera98@gmail.com>
2025-09-08 08:06:41 +0200

    tweaks

--- common/data/utils.py
+++ common/data/utils.py
@@ -4,26 +4,6 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 
-class BoundedMap(OrderedDict):
-    def __init__(self, max_length: int, lru: bool = False):
-        super().__init__()
-        self.max_length, self.lru = max_length, lru
-
-    def __getitem__(self, key):
-        v = super().__getitem__(key)
-        if self.lru:
-            self.move_to_end(key)  # mark as most-recent
-        return v
-
-    def __setitem__(self, key, value):
-        if key in self and self.lru:
-            self.move_to_end(key)
-        super().__setitem__(key, value)
-        if len(self) > self.max_length:
-            # Evict the oldest entry
-            self.popitem(last=False)
-
-
 def dataset_information(dataset: Dataset, image_size: tuple[int, int]) -> tuple[torch.Tensor, torch.Tensor]:
     """
     Returns the mean and variance of the data.
