035953a52bde34014f7cb57ff69c0e0790e48a0e
jfichera@imteam.it <jfichera@imteam.it>
2025-09-29 09:18:15 +0200

    Pipeline

--- core_data/extract.py
+++ core_data/extract.py
@@ -1,9 +1,41 @@
 from abc import ABC, abstractmethod
 
+import pandas as pd
+
 from core_data.data_point import FlexibleDatasetPoint
+from core_data.loader import DataPointsLoader
+from core_data.sampler import Segmenter
 
 
 class Extractor(ABC):
     @abstractmethod
     def extract(self, x: FlexibleDatasetPoint) -> str:
         pass
+
+
+class SegmentBasedExtractionProcessor:
+    def __init__(self, base_path: str, segmenter: Segmenter, loader: DataPointsLoader, *other_extractors: Extractor):
+        self.segmenter: Segmenter = segmenter
+        self.points_loader: DataPointsLoader = loader
+        # Custom functions to be applied
+        self.other_extractors: tuple[Extractor, ...] = other_extractors
+        self.base_path: str = base_path
+
+        self.seen = []
+
+    def extract_segments(self) -> list[list[str]]:
+        outs = []
+        for x in self.points_loader.scan():
+            key = x.get_identifier()
+            if key in self.seen:
+                continue
+
+            segments: list[tuple[float, float]] = self.segmenter.compute_segments(x)
+            local_outs = [o.extract(x) for o in self.other_extractors]
+
+            df = pd.DataFrame(segments, columns=["start", "stop"])
+            df.to_csv(f"{self.base_path}{x.eid}-segments.csv", index=False)
+            local_outs.append(f"{self.base_path}{x.eid}-segments.csv")
+            outs.append(local_outs)
+
+        return outs
--- dataset/amigos/scripts/interval_sampler.py
+++ dataset/amigos/scripts/interval_sampler.py
@@ -1,31 +1,3 @@
-import pandas as pd
-
-from core_data.extract import Extractor
-from core_data.loader import DataPointsLoader
-from core_data.sampler import Segmenter
 
 
-class SegmentsExtractor:
-    def __init__(self, base_path: str, segmenter: Segmenter, points_loader: DataPointsLoader,
-                 *other_extractors: Extractor):
-        self.segmenter: Segmenter = segmenter
-        self.points_loader: DataPointsLoader = points_loader
 
-        # Custom functions to be applied
-        self.other_extractors = other_extractors
-        self.base_path: str = base_path
-
-        self.seen = []
-
-    def extract_segments(self):
-        for x in self.points_loader.scan():
-            key = x.get_identifier()
-            if key in self.seen:
-                continue
-
-            segments: list[tuple[float, float]] = self.segmenter.compute_segments(x)
-            outs = [o.extract(x) for o in self.other_extractors]
-            print(outs)
-
-            df = pd.DataFrame(segments, columns=["start", "stop"])
-            df.to_csv(f"{self.base_path}{x.eid}-segments.csv", index=False)
