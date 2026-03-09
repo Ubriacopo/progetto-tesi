994c7e1d7e313079a67f9aaded19fe740b2d9048
jacopo <jacopo.fichera98@gmail.com>
2026-03-09 23:45:39 +0100

    seeding

--- main/model/script/train.py
+++ main/model/script/train.py
@@ -1,5 +1,5 @@
 import hydra
-import torch
+import lightning
 import torchinfo
 from lightning.pytorch.profilers import SimpleProfiler
 
@@ -10,8 +10,7 @@ from main.utils.logging import make_logger
 
 @hydra.main(config_path="../../../conf", config_name="train")
 def main(cfg: KdConfig):
-    # cfg = OmegaConf.to_container(cfg, resolve=True)
-    torch.manual_seed(cfg.seed)  # Reproducibility
+    lightning.seed_everything(cfg.seed, workers=True)
     logger = make_logger("hydra-main.train")
     module = build_easy_eegavi_module(cfg)
     torchinfo.summary(module)
@@ -24,6 +23,7 @@ def main(cfg: KdConfig):
 
     profiling = False
     profiler = SimpleProfiler() if profiling else None
+
     trainer = default_trainer(
         epochs=40,
         model_name=model_name,
