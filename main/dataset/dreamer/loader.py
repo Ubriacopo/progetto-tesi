319cc8206b79ff0a174399294645780b4d20a663
jacopo <jacopo.fichera98@gmail.com>
2026-03-12 18:15:23 +0100

    removed useless stuff and wip dreamer

--- main/dataset/dreamer/loader.py
+++ main/dataset/dreamer/loader.py
@@ -1,21 +1,83 @@
+from dataclasses import asdict
 from pathlib import Path
 from typing import Iterator
 
+import mne
+from mne.io import RawArray
+from scipy.io import loadmat
+
 from main.core_data.data_point import FlexibleDatasetPoint
 from main.core_data.loader import DataPointsLoader
+from main.core_data.media.ecg import ECG
+from main.core_data.media.eeg import EEG
+from main.core_data.media.metadata.metadata import MetaObject, Metadata
+from main.dataset.dreamer.config import DreamerConfig
 from main.dataset.utils import DatasetUidStore
 
 
 class DreamerPointsLoader(DataPointsLoader):
-    def __init__(self, base_path: str, dataset_uid_store: DatasetUidStore):
+    DATASET_ID: int = 5
+
+    def __init__(self, base_path: str, dataset_uid_store: DatasetUidStore, config: DreamerConfig = DreamerConfig()):
         super().__init__(dataset_uid_store)
         self.base_path = base_path
+        self.config: DreamerConfig = config
 
     def __len__(self) -> int:
-        pass
+        if self.length is not None:
+            return self.length
+
+        dreamer = loadmat(f"{self.base_path}/DREAMER.mat")["DREAMER"][0][0]
 
     # TODO finish
     def scan(self) -> Iterator[FlexibleDatasetPoint]:
         processed_data = Path(self.base_path + "data_preprocessed_python/")
+        dreamer = loadmat(f"{self.base_path}/DREAMER.mat")["DREAMER"][0][0]
+        # Dreamer has 10 columns. Of these last is useless.
+        # dreamer[0] Contains Age, Gender, EEG (index?), Score Valance, Score Arousal Score Dominance
+        # [('Age', 'O'), ('Gender', 'O'), ('EEG', 'O'), ('ECG', 'O'), ('ScoreValence', 'O'), ('ScoreArousal', 'O'), ('ScoreDominance', 'O')]
+        # dreamer[3] is the order of EEG channels?
+        # After dreamer 6 (Compreso) useless
 
-        yield FlexibleDatasetPoint()
+        # Count number of experiments
+        users = dreamer[0].shape[1]
+        for user in range(users):
+            user_experiment_data = dreamer[0][user]
+
+            # Experiments with index
+            for experiment_index in range(len(user_experiment_data["ScoreValence"][0].squeeze())):
+                # TODO Compose
+                nei = self.dataset_uid_store.uid(str(user), str(experiment_index), "DREAMER")
+                # EEG Data: Stimuli is what matters to us. The normalization of bias is not of help? Guess not
+                eeg_np = user_experiment_data["EEG"][0][0]["stimuli"][0][experiment_index][0]  # Time x Channels
+                info = mne.create_info(
+                    # todo fill
+                    ch_names=self.config.eeg_source_config.get_CH_NAMES(),
+                    ch_types=self.config.eeg_source_config.get_CH_TYPES(),
+                    sfreq=self.config.eeg_source_config.fs
+                )
+                raw: RawArray = mne.io.RawArray(eeg_np, info=info, verbose=False)
+
+                ecg_np = user_experiment_data["ECG"][0][0]["stimuli"][0][experiment_index][0]
+                # todo studia su tutti i dataset per decidere obiettivo
+                valence = user_experiment_data["ScoreValence"][0].squeeze()[experiment_index]
+                arousal = user_experiment_data["ScoreArousal"][0].squeeze()[experiment_index]
+                dominance = user_experiment_data["ScoreDominance"][0].squeeze()[experiment_index]
+
+                metadata = MetaObject(
+                    experiment=nei, dataset_id=self.DATASET_ID, person_id=user, trial=experiment_index
+                )
+
+                yield FlexibleDatasetPoint(
+                    nei,
+                    EEG(eid=nei, data=raw.copy().pick(["eeg"]), fs=raw.info['sfreq']).as_mod_tuple(),
+                    ECG(
+                        eid=nei,
+                        data=raw.copy().pick(["ecg"]),
+                        fs=raw.info['sfreq'],
+                        leads=self.config.ecg_source_config.LEAD_NAMES,  # todo fai anche questo
+                        patient_gender=user_experiment_data["Gender"][0].upper(),
+                        patient_age=user_experiment_data["Age"][0]
+                    ).as_mod_tuple(),
+                    Metadata(data=asdict(metadata), eid=nei).as_mod_tuple()
+                )
--- main/dataset/script/prepare_ds_pre_extracted/README.md
+++
@@ -1,2 +0,0 @@
-To run multiple in sequence (So you don't have to touch anything):
-> -m dataset=amigos,manhob,eav target_model=eegavi,vate --config-name=local
